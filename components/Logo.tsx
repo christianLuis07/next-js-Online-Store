bae89ea5a4bd561766185eb305e48e6f1820969c
Christian <christianluisginting@gmail.com>
2025-06-16 20:28:41 +0700

    finishing:header section

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,13 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 
-const Logo = ({ className }: { className?: string }) => {
+const Logo = ({
+  className,
+  spanDesign,
+}: {
+  className?: string;
+  spanDesign?: string;
+}) => {
   return (
     <Link href={"/"}>
       <h2
@@ -11,7 +17,12 @@ const Logo = ({ className }: { className?: string }) => {
           className
         )}
       >
-        <span className="text-shop_light_green group-hover:text-shop_dark_green hoverEffect">
+        <span
+          className={cn(
+            "text-shop_light_green group-hover:text-shop_dark_green hoverEffect",
+            spanDesign
+          )}
+        >
           C
         </span>
         Shop
