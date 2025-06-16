7b2fbc5e99c9fefefd2cb55b00d0c1077aa9e724
Christian <christianluisginting@gmail.com>
2025-06-16 17:28:51 +0700

    add:header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="">
+    <Container className="">
       <h2 className="text-xl font-semibold">Home</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
         asperiores maiores dignissimos blanditiis magnam.
       </p>
       <Button>Check Out</Button>
-    </div>
+    </Container>
   );
 };
 
