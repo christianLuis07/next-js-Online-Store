656f339a0d376c2ea463619e20fcb7f0c54910d3
Christian <christianluisginting@gmail.com>
2025-06-17 13:36:43 +0700

    add:Home Banner Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
     <Container className="">
-      <h2 className="text-xl font-semibold">Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-        asperiores maiores dignissimos blanditiis magnam.
-      </p>
-      <Button>Check Out</Button>
+      <HomeBanner />
     </Container>
   );
 };
