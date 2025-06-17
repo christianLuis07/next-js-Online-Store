57c31f2924f5ff66bf56aee785f2f71221b76379
Christian <christianluisginting@gmail.com>
2025-06-17 13:37:09 +0700

    Add:Home Banner Section

---
+++ components/ui/text.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+
+export const Title = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h2
+      className={cn(
+        "text-3xl mb-5 font-bold text-shop_dark_green capitalize tracking-wide font-sans",
+        className
+      )}
+    >
+      {children}
+    </h2>
+  );
+};
