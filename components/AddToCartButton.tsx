8ac108b6e5373084da4fb041c6e94d926a100e2b
Christian <christianluisginting@gmail.com>
2025-06-18 20:47:51 +0700

    add:new component

---
+++ components/AddToCartButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Product } from "@/sanity.types";
+import React from "react";
+import { Button } from "./ui/button";
+import { ShoppingBag } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  product: Product;
+  className?: string;
+}
+
+const AddToCartButton = ({ product, className }: Props) => {
+  const isOutOfStock = product?.stock === 0;
+  const handleAddToCart = () => {
+    window.alert("Produk berhasil ditambahkan ke keranjang");
+  };
+  return (
+    <div>
+      <Button
+        onClick={handleAddToCart}
+        disabled={isOutOfStock}
+        className={cn(
+          "w=full bg-shop_dark_green/80 text-shop_light_bg shadow-none border border-shop_dark_green/80 font-semibold tracking-wide hover:text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect",
+          className
+        )}
+      >
+        <ShoppingBag /> {isOutOfStock ? "Habis" : "Tambah ke Keranjang"}
+      </Button>
+    </div>
+  );
+};
+
+export default AddToCartButton;
