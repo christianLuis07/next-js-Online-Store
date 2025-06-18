a8c83b085774d36004140b4f5e9821d51508c702
Christian <christianluisginting@gmail.com>
2025-06-18 14:01:03 +0700

    edit:schema

--- sanity/schemaTypes/productType.ts
+++ sanity/schemaTypes/productType.ts
@@ -3,13 +3,13 @@ import { defineField, defineType } from "sanity";
 
 export const productType = defineType({
   name: "product",
-  title: "Products",
+  title: "Produk",
   type: "document",
   icon: TrolleyIcon,
   fields: [
     defineField({
       name: "name",
-      title: "Product Name",
+      title: "Nama Produk",
       type: "string",
       validation: (Rule) => Rule.required(),
     }),
@@ -25,76 +25,75 @@ export const productType = defineType({
     }),
     defineField({
       name: "images",
-      title: "Product Images",
+      title: "Gambar Produk",
       type: "array",
       of: [{ type: "image", options: { hotspot: true } }],
     }),
     defineField({
       name: "description",
-      title: "Description",
+      title: "Deskripsi",
       type: "string",
     }),
     defineField({
       name: "price",
-      title: "Price",
+      title: "Harga",
       type: "number",
       validation: (Rule) => Rule.required().min(0),
     }),
     defineField({
       name: "discount",
-      title: "Discount",
+      title: "Diskon",
       type: "number",
       validation: (Rule) => Rule.required().min(0),
     }),
     defineField({
       name: "categories",
-      title: "Categories",
+      title: "Kategori",
       type: "array",
       of: [{ type: "reference", to: { type: "category" } }],
     }),
     defineField({
       name: "stock",
-      title: "Stock",
+      title: "Stok",
       type: "number",
       validation: (Rule) => Rule.min(0),
     }),
     defineField({
       name: "brand",
-      title: "Brand",
+      title: "Merek",
       type: "reference",
       to: { type: "brand" },
     }),
-
     defineField({
       name: "status",
-      title: "Product Status",
+      title: "Status Produk",
       type: "string",
       options: {
         list: [
-          { title: "New", value: "new" },
-          { title: "Hot", value: "hot" },
-          { title: "Sale", value: "sale" },
+          { title: "Baru", value: "new" },
+          { title: "Terlaris", value: "hot" },
+          { title: "Diskon", value: "sale" },
         ],
       },
     }),
     defineField({
       name: "variant",
-      title: "Product Type",
+      title: "Jenis Produk",
       type: "string",
       options: {
         list: [
           { title: "Gadget", value: "gadget" },
-          { title: "Appliances", value: "appliances" },
-          { title: "Refrigerators", value: "refrigerators" },
-          { title: "Others", value: "others" },
+          { title: "Peralatan", value: "appliances" },
+          { title: "Kulkas", value: "refrigerators" },
+          { title: "Lainnya", value: "others" },
         ],
       },
     }),
     defineField({
       name: "isFeatured",
-      title: "Featured Product",
+      title: "Produk Unggulan",
       type: "boolean",
-      description: "Toggle to Featured on or off",
+      description: "Aktifkan jika ingin menampilkan sebagai produk unggulan",
       initialValue: false,
     }),
   ],
@@ -107,9 +106,13 @@ export const productType = defineType({
     prepare(selection) {
       const { title, subtitle, media } = selection;
       const image = media && media[0];
+
+      // Format angka ke Rupiah dengan titik
+      const formattedPrice = new Intl.NumberFormat("id-ID").format(subtitle);
+
       return {
         title: title,
-        subtitle: `$${subtitle}`,
+        subtitle: `Rp${formattedPrice}`,
         media: image,
       };
     },
