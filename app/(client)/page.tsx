5938acba7baeae13a592fac61ac0f5dcf0c28c22
Christian <christianluisginting@gmail.com>
2025-06-18 19:15:43 +0700

    modified:sanity

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import ProductGrid from "@/components/ProductGrid";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
@@ -7,6 +8,9 @@ const Home = () => {
   return (
     <Container className="">
       <HomeBanner />
+      <div className="py-10">
+        <ProductGrid />
+      </div>
     </Container>
   );
 };
