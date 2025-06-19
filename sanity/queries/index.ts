aa156e38e4e90c670bb472ad80a9557dd06d2b33
Christian <christianluisginting@gmail.com>
2025-06-19 12:18:13 +0700

    add:queries

---
+++ sanity/queries/index.ts
@@ -0,0 +1,25 @@
+import { sanityFetch } from "../lib/live";
+
+const getCategories = async (quantity?: number) => {
+  try {
+    const query = quantity
+      ? `*[_type == 'category'] | order(name asc) [0...$quantity] {
+          ...,
+          "productCount": count(*[_type == "product" && references(^._id)])
+        }`
+      : `*[_type == 'category'] | order(name asc) {
+          ...,
+          "productCount": count(*[_type == "product" && references(^._id)])
+        }`;
+    const { data } = await sanityFetch({
+      query,
+      params: quantity ? { quantity } : {},
+    });
+    return data;
+  } catch (error: any) {
+    console.error("Failed to fetch categories:", error); // tampilkan detail error
+    throw new Error(`Failed to fetch categories: ${error.message}`);
+  }
+};
+
+export { getCategories };
