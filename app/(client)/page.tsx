35a2280f76b008a5ed2c634e96b90a32ab295496
Christian <christianluisginting@gmail.com>
2025-06-19 12:18:52 +0700

    edit:page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,16 +1,20 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import HomeCategories from "@/components/HomeCategories";
 import ProductGrid from "@/components/ProductGrid";
 import { Button } from "@/components/ui/button";
+import { getCategories } from "@/sanity/queries";
 import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const categories = await getCategories(6);
   return (
     <Container className="">
       <HomeBanner />
       <div className="py-10">
         <ProductGrid />
       </div>
+      <HomeCategories categories={categories} />
     </Container>
   );
 };
