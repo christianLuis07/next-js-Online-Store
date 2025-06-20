9653c1d8973dd0634a232b8d54a603a226d1803a
Christian <christianluisginting@gmail.com>
2025-06-20 13:20:56 +0700

    modified:fetch category by slug

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,7 +1,29 @@
+import CategoryProducts from "@/components/CategoryProducts";
+import Container from "@/components/Container";
+import { Title } from "@/components/ui/text";
+import { getCategories } from "@/sanity/queries";
 import React from "react";
 
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+const CategoryPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const categories = await getCategories();
+  const { slug } = await params;
+  return (
+    <div>
+      <Container className="py-10 ">
+        <Title>
+          Product dari Kategori:{" "}
+          <span className="font-bold text-green-600 capitalize tracking-wide">
+            {slug && slug}
+          </span>
+        </Title>
+        <CategoryProducts categories={categories} slug={slug} />
+      </Container>
+    </div>
+  );
 };
 
 export default CategoryPage;
