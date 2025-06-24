cbacdfb21848be8b6dfa20b91ce31517157a3b1f
Christian <christianluisginting@gmail.com>
2025-06-24 07:20:24 +0700

    update:add to cart

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -1,10 +1,12 @@
 "use client";
-
 import { Product } from "@/sanity.types";
-import React from "react";
 import { Button } from "./ui/button";
-import { ShoppingBag } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { ShoppingBag } from "lucide-react";
+import useStore from "@/store";
+import toast from "react-hot-toast";
+import PriceFormattor from "./PriceFormattor";
+import QuantityButtons from "./QuantityButtons";
 
 interface Props {
   product: Product;
@@ -12,22 +14,47 @@ interface Props {
 }
 
 const AddToCartButton = ({ product, className }: Props) => {
+  const { addItem, getItemCount } = useStore();
+  const itemCount = getItemCount(product?._id);
   const isOutOfStock = product?.stock === 0;
+
   const handleAddToCart = () => {
-    window.alert("Produk berhasil ditambahkan ke keranjang ");
+    if ((product?.stock as number) > itemCount) {
+      addItem(product);
+      toast.success(
+        `${product?.name?.substring(0, 12)}... added successfully!`
+      );
+    } else {
+      toast.error("Can not add more than available stock");
+    }
   };
   return (
-    <div>
-      <Button
-        onClick={handleAddToCart}
-        disabled={isOutOfStock}
-        className={cn(
-          "w=full bg-shop_dark_green/80 text-shop_light_bg shadow-none border border-shop_dark_green/80 font-semibold tracking-wide hover:text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect",
-          className
-        )}
-      >
-        <ShoppingBag /> {isOutOfStock ? "Habis" : "Keranjang"}
-      </Button>
+    <div className="w-full h-12 flex items-center">
+      {itemCount ? (
+        <div className="text-sm w-full">
+          <div className="flex items-center justify-between">
+            <span className="text-xs text-darkColor/80">Quantity</span>
+            <QuantityButtons product={product} />
+          </div>
+          <div className="flex items-center justify-between border-t pt-1">
+            <span className="text-xs font-semibold">Subtotal</span>
+            <PriceFormattor
+              amount={product?.price ? product?.price * itemCount : 0}
+            />
+          </div>
+        </div>
+      ) : (
+        <Button
+          onClick={handleAddToCart}
+          disabled={isOutOfStock}
+          className={cn(
+            "w-full bg-shop_dark_green/80 text-lightBg shadow-none border border-shop_dark_green/80 font-semibold tracking-wide text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect",
+            className
+          )}
+        >
+          <ShoppingBag /> {isOutOfStock ? "Out of Stock" : "Add to Cart"}
+        </Button>
+      )}
     </div>
   );
 };
